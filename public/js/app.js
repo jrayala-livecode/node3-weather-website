d6d27625ab8e095de75e347ef78b5798536cfc4d
tangentkerching <joaquin.ayala.c@gmail.com>
2020-04-09 10:59:20 -0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     if(location === ''){
         messageOne.textContent = 'Fill the search field first.';
     }else{
-        fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        fetch('/weather?address='+location).then((response) => {
             response.json().then((data) =>{
                 
                 if(data.error){
